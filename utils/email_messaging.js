672c86c352385d2ab46570cbde9985c1d34b2ad5
drex007 <vykeoj@gmail.com>
2023-12-11 17:05:04 +0000

    added emailing system

---
+++ utils/email_messaging.js
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+let transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
+
+export const sendEmail = async (receiver, vc) => {
+  const message = await transport.sendMail({
+    from: "airove@example.com",
+    to: receiver,
+    subject: "Ticket purchase",
+    text: vc,
+    html: `<b>Hello ${receiver}</b>`
+  })
+
+  return message.messageId
+}
