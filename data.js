2dcecf36268148d7b5048b3f9ddfa743badbb608
drex007 <vykeoj@gmail.com>
2023-12-11 06:48:36 +0000

    edited data file

--- data.js
+++ data.js
@@ -1,10 +1,2 @@
-module.exports = [
-  {
-    "customer_did": "djjdjdjdjjdjdj",
-    "wallet_address": "3KZbnwCtxSCuYHYympLTYTXy9j34puEDEv",
-    "ticket_data": {
-      "price_amount": 120,
-      "customer_did": "djjdjdjdjjdjdj"
-    }
-  }
-];
+let myArray = []
+export { myArray }
